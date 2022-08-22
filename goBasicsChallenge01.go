259f3b830bcb3fae0810883b844968e54c3f2e4e
Raj <rtekal@users.noreply.github.com>
2022-08-22 18:22:40 +0000

    learning about go basics

---
+++ goBasicsChallenge01.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+  "fmt"
+)
+
+
+func main() {
+  var city, state string = "Quakertown", "PA"
+  fmt.Println("My name is Raj Tekal")
+  fmt.Println("I am from ", city + ", " + state)
+}
