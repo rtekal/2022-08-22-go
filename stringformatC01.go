9fb737216278063c47c7171b40e10ce14acb1d6b
Raj <rtekal@users.noreply.github.com>
2022-08-23 14:27:17 +0000

    learning about go array

--- stringformatC01.go
+++ stringformatC01.go
@@ -6,13 +6,13 @@ import (
 
 func main() {
  var (
-  uri string = "https://example.org:6001/v2/snacks?"
-  r   string = "req=snickers"
-  q   string = "quantity=1"
-  s   string = "size=king"
+    uri string = "https://example.org:6001/v2/snacks?"
+    r   string = "req=snickers"
+    q   string = "quantity=1"
+    s   string = "size=king"
   )
 
-  furi := fmt.Sprintf("%s%s&%s&%s\n",uri, r, q, s)
+  furi := fmt.Sprintf("%s%s&%s&%s",uri, r, q, s)
 
   fmt.Println(furi)
 }
